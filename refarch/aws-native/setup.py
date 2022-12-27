f43af31812c0498c5eef97ffb0b0edb6bc2cf1db
Begum Firdousi Abbas <fibeg@amazon.com>
2022-12-27 13:05:03 +0100

    Pushing new branch

--- refarch/aws-native/setup.py
+++ refarch/aws-native/setup.py
@@ -7,7 +7,7 @@ with open("README.md") as fp:
 
 setuptools.setup(
     name="analytics-reference-architecture",
-    version="2.4.12",
+    version="2.5.1",
 
     description="The Analytics Reference Architecture CDK app",
     long_description=long_description,
@@ -19,7 +19,7 @@ setuptools.setup(
     packages=setuptools.find_packages(where="common"),
 
     install_requires=[
-        "aws_analytics_reference_architecture==2.4.14",
+        "aws_analytics_reference_architecture==2.5.1",
         "aws-cdk-lib>=2.26.0",
         "aws-cdk.aws-glue-alpha>=2.26.0.a0"
         "aws-cdk.aws-redshift-alpha>=2.26.0.a0"
